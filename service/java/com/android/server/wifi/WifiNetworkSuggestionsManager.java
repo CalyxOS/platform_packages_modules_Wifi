6fbaa0f62cb765c33dc9cb7c76fd3fb8a313be5f
Jimmy Chen <jimmycmchen@google.com>
2022-08-12 12:06:55 +0800

    [DO NOT MERGE] wifi: remove certificates for network factory reset
    When a user does network factory reset, wifi certificate should
    also be cleared as these certificates are also personal credentials.
    
    Bug: 231985227
    Test: atest FrameworksWifiTests
    Merged-In: I0334064347080e43096d01f00cf516804812d0c0
    Change-Id: I0334064347080e43096d01f00cf516804812d0c0
    (cherry picked from commit ed098227320050d3db0b8482ff3298ae11fe1777)
    Merged-In: I0334064347080e43096d01f00cf516804812d0c0

--- service/java/com/android/server/wifi/WifiNetworkSuggestionsManager.java
+++ service/java/com/android/server/wifi/WifiNetworkSuggestionsManager.java
@@ -1326,7 +1326,7 @@ public class WifiNetworkSuggestionsManager {
             removeFromPassPointInfoMap(ewns);
         } else {
             if (ewns.wns.wifiConfiguration.isEnterprise()) {
-                mWifiKeyStore.removeKeys(ewns.wns.wifiConfiguration.enterpriseConfig);
+                mWifiKeyStore.removeKeys(ewns.wns.wifiConfiguration.enterpriseConfig, false);
             }
             removeFromScanResultMatchInfoMapAndRemoveRelatedScoreCard(ewns, true);
             mWifiConfigManager.removeConnectChoiceFromAllNetworks(ewns
